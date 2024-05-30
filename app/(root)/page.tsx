50528b3a4bc69e379315646fd7867b9c825713db
suleman ahmed <ahmedsuleman@outlook.dk>
2024-05-30 13:45:34 +0200

    Implement components AnimatedCounter DoughtnutChart HeaderBox and TotalBalanceBox for the main page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox"
+import TotalBalanceBox from "@/components/TotalBalanceBox"
 
 const Home = () => {
+  const loggedIn = { firstname: 'Guest'}
   return (
-    <div>Home</div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox 
+          type="greeting"
+          title="Welcome"
+          user={loggedIn?.firstname || 'Guest'}
+          subtext="Access and manage your account and transactions efficiently."
+          />
+
+          <TotalBalanceBox 
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
+    </section>
   )
 }
 
